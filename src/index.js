05a9cc09ccde150da246ff6ebd5cd49bbb04d76a
Robert Ionescu <hightronicdesign@gmail.com>
2017-10-16 18:49:07 +0200

    added package-lock.json

--- src/index.js
+++ src/index.js
@@ -11,6 +11,17 @@ import App from './App/Components/App'
 import About from './App/Components/About'
 import SubmitHash from './App/Components/SubmitHash'
 
+// Setup basic express server
+var express = require('express');
+var app = express();
+var path = require('path');
+var server = require('http').createServer(app);
+var io = require('../..')(server);
+var port = process.env.PORT || 3000;
+
+server.listen(port, function () {
+    console.log('Server listening at port %d', port);
+});
 
 ReactDOM.render(
     <BrowserRouter>
